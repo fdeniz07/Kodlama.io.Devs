900708ca639cc98eaced7fe8b218945c021a2cc6
Fatih Deniz <deniz.fatih@hotmail.de>
2023-03-20 00:28:40 +0100

    7th Day

---
+++ devs/src/main/java/kodlama/io/devs/core/utilities/exceptions/BusinessException.java
@@ -0,0 +1,8 @@
+package kodlama.io.devs.core.utilities.exceptions;
+
+public class BusinessException extends RuntimeException {
+
+    public BusinessException(String message) {
+        super(message);
+    }
+}
