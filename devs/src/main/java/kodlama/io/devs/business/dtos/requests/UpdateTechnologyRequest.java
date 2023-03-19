4119a299ab09a9457edb8bce85b9e5e4da123670
Fatih Deniz <deniz.fatih@hotmail.de>
2023-03-20 00:28:14 +0100

    7th Day

---
+++ devs/src/main/java/kodlama/io/devs/business/dtos/requests/UpdateTechnologyRequest.java
@@ -0,0 +1,23 @@
+package kodlama.io.devs.business.dtos.requests;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UpdateTechnologyRequest {
+
+    private int id;
+
+    @NotNull
+    @NotBlank
+    @Size(min=2,max = 20)
+    private String name;
+
+    private int programmingLanguageId;
+}
