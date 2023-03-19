1754b3898739a6ca708abbe9a6262e03831cec4c
Fatih Deniz <deniz.fatih@hotmail.de>
2023-03-20 00:27:00 +0100

    7th Day

--- devs/src/main/java/kodlama/io/devs/dataAccess/abstracts/TechnologyRepository.java
+++ devs/src/main/java/kodlama/io/devs/dataAccess/abstracts/TechnologyRepository.java
@@ -5,4 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TechnologyRepository extends JpaRepository<Technology,Integer> {
 
+    boolean existsByName(String name);
 }
